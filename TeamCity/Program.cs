11659bea5279df45aedfe3480e4c6fc208b2f4ae
Roman <roma-zateev@mail.ru>
2020-03-30 20:21:51 +0700

    first commit

---
+++ TeamCity/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeamCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("res: " + count(i, i+1).ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int count(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}   
